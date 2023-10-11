4f4f647e1d8e7f5fa48308da52bcd4a32b55a52e
Pham Tu <tubackkhoa@gmail.com>
2023-10-11 16:34:12 +0700

    update code

--- gravity-info-dash/src/components/IncommingTransactions.tsx
+++ gravity-info-dash/src/components/IncommingTransactions.tsx
@@ -1,11 +1,6 @@
 import { Table, Card, CardBody, CardTitle } from 'reactstrap';
 import { DepositWithMetadata, Erc20Metadata, EthInfo } from '../types';
-import {
-  amountToFraction,
-  cosmosAddressToExplorerLink,
-  getMetadataFromList,
-  printTxStatus
-} from '../utils';
+import { amountToFraction, cosmosAddressToExplorerLink, getMetadataFromList, printTxStatus } from '../utils';
 
 interface Props {
   ethBridgeInfo: EthInfo;
@@ -13,22 +8,12 @@ interface Props {
   erc20Metadata: Array<Erc20Metadata>;
 }
 
-export const IncommingTransactions: React.FC<Props> = ({
-  ethBridgeInfo,
-  etherscanBase,
-  erc20Metadata
-}) => {
+export const IncommingTransactions: React.FC<Props> = ({ ethBridgeInfo, etherscanBase, erc20Metadata }) => {
   return (
     <Card className="ParametersCard" style={{ borderRadius: 8, padding: 20 }}>
       <CardBody>
         <CardTitle tag="h4">Incoming transactions</CardTitle>
-        <Table
-          dark
-          borderless
-          size="sm"
-          responsive
-          style={{ borderSpacing: 10, fontSize: 15 }}
-        >
+        <Table dark borderless size="sm" responsive style={{ borderSpacing: 10, fontSize: 15 }}>
           <thead>
             <tr>
               <th>Token</th>
@@ -39,40 +24,23 @@ export const IncommingTransactions: React.FC<Props> = ({
             </tr>
           </thead>
           <tbody>
-            {ethBridgeInfo.deposit_events.map(
-              (sendToCosmos: DepositWithMetadata) => (
-                <tr key={sendToCosmos.event_nonce}>
-                  <td>
-                    {
-                      getMetadataFromList(sendToCosmos.erc20, erc20Metadata)
-                        ?.symbol
-                    }
-                  </td>
-                  <td>
-                    {amountToFraction(
-                      sendToCosmos.erc20,
-                      sendToCosmos.amount,
-                      erc20Metadata
-                    )}
-                  </td>
-                  <td>
-                    <a href={`${etherscanBase}/address/${sendToCosmos.sender}`}>
-                      {sendToCosmos.sender}
-                    </a>
-                  </td>
-                  <td>
-                    <a
-                      href={cosmosAddressToExplorerLink(
-                        sendToCosmos.destination
-                      )}
-                    >
-                      {sendToCosmos.destination}
-                    </a>
-                  </td>
-                  <td>{printTxStatus(sendToCosmos)}</td>
-                </tr>
-              )
-            )}
+            {ethBridgeInfo.deposit_events.map((sendToCosmos: DepositWithMetadata) => (
+              <tr key={sendToCosmos.event_nonce}>
+                <td>{getMetadataFromList(sendToCosmos.erc20, erc20Metadata)?.symbol}</td>
+                <td>{amountToFraction(sendToCosmos.erc20, sendToCosmos.amount, erc20Metadata)}</td>
+                <td>
+                  <a target="_blank" href={`${etherscanBase}/address/${sendToCosmos.sender}`}>
+                    {sendToCosmos.sender}
+                  </a>
+                </td>
+                <td>
+                  <a target="_blank" href={cosmosAddressToExplorerLink(sendToCosmos.destination)}>
+                    {sendToCosmos.destination}
+                  </a>
+                </td>
+                <td>{printTxStatus(sendToCosmos)}</td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </CardBody>
--- gravity-info-dash/src/utils.ts
+++ gravity-info-dash/src/utils.ts
@@ -1,18 +1,9 @@
 /// Inefficient utility function to lookup token metadata, should be using a map
 
-import {
-  Attestation,
-  DepositWithMetadata,
-  Erc20Metadata,
-  EthInfo,
-  GravityInfo
-} from './types';
+import { Attestation, DepositWithMetadata, Erc20Metadata, EthInfo, GravityInfo } from './types';
 
 /// of some kind
-export function getMetadataFromList(
-  erc20: string,
-  metadata: Array<Erc20Metadata>
-) {
+export function getMetadataFromList(erc20: string, metadata: Array<Erc20Metadata>) {
   var arrayLength = metadata.length;
   for (var i = 0; i < arrayLength; i++) {
     if (metadata[i].address === erc20) {
@@ -24,11 +15,7 @@ export function getMetadataFromList(
 
 /// returns a readable fraction value for a given erc20 amount, if the exchange rate is populated
 /// it is used to display token value / dollar value
-export function amountToFraction(
-  erc20: string,
-  amount: number,
-  metadata: Array<Erc20Metadata>
-) {
+export function amountToFraction(erc20: string, amount: number, metadata: Array<Erc20Metadata>) {
   let tokenInfo = getMetadataFromList(erc20, metadata);
   if (tokenInfo == null) {
     return 0;
@@ -45,19 +32,11 @@ export function amountToFraction(
 /// Takes both info structs to cross compare and display batches that have not yet been
 /// executed without waiting 20 minutes for Gravity to know that a batch has been executed
 /// on Ethereum
-export function getNotExecutedBatches(
-  gravityBridgeInfo: GravityInfo,
-  ethBridgeInfo: EthInfo
-) {
+export function getNotExecutedBatches(gravityBridgeInfo: GravityInfo, ethBridgeInfo: EthInfo) {
   let ret = [];
   var arrayLength = gravityBridgeInfo.pending_batches.length;
   for (var i = 0; i < arrayLength; i++) {
-    if (
-      !alreadyExecuted(
-        gravityBridgeInfo.pending_batches[i].nonce,
-        ethBridgeInfo
-      )
-    ) {
+    if (!alreadyExecuted(gravityBridgeInfo.pending_batches[i].nonce, ethBridgeInfo)) {
       ret.push(gravityBridgeInfo.pending_batches[i]);
     }
   }
@@ -90,7 +69,7 @@ const prefixBaseMap: { [key: string]: string } = {
 /// Takes various cosmos addresses to create a proper mintscan link
 export const cosmosAddressToExplorerLink = (input: string): string => {
   const prefix = input.split('1')[0];
-  return prefixBaseMap[prefix] || input;
+  return prefixBaseMap[prefix] + input;
 };
 
 export const getEtherScanBase = (evmChainPrefix?: string): string => {
